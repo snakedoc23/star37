7545d7ccc3d93955952c84774dcafd404567f593
snakedoc23 <snakedoc23@gmail.com>
2011-11-12 20:18:02 +0100

    icons, sass styling, edit link, search links

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,7 +1,7 @@
 class LinksController < ApplicationController
 
 	def index
-		@links = Link.all
+		@links = Link.search params[:search]
 		@link = Link.new
 	end
 
@@ -9,11 +9,35 @@ class LinksController < ApplicationController
 
 		@link = Link.new params[:link]
 		if @link.save
+			flash[:success] = "Yeah"
 			redirect_to links_path
 		else
 			@links = Link.all
+			flash[:error] = "Nope"
 			render 'index'
 		end
 	end
+
+	def edit
+		@link = Link.find params[:id]
+	end
+
+	def update
+		@link = Link.find params[:id]
+		if @link.update_attributes params[:link]
+			flash[:success] = "Yeah"
+			redirect_to links_path
+		else
+			flash[:error] = "Nope"
+			render 'edit'
+		end
+	end
+
+  def destroy
+    Link.find(params[:id]).destroy
+    flash[:success] = "Link deleted"
+    redirect_to links_path
+    
+  end
 
 end
--- app/models/link.rb
+++ app/models/link.rb
@@ -1,4 +1,12 @@
 class Link < ActiveRecord::Base
 
 	validates :content, :presence => true
+
+	def self.search(search)
+		if search
+			where('content LIKE ?', "%#{search}%")
+		else
+			scoped
+		end
+	end
 end
